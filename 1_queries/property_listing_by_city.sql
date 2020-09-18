20267477db6d5f1aebcb47693018a6fbbf1ed2f2
SarahAlAshwal <sa.alashwal@gmail.com>
2020-09-18 01:49:32 +0000

    add a property to the database

--- 1_queries/property_listing_by_city.sql
+++ 1_queries/property_listing_by_city.sql
@@ -5,3 +5,6 @@ GROUP BY  properties.id
 HAVING AVG (rating) >= 4
 ORDER BY cost_per_night
 LIMIT 10;
+
+
+
--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -84,9 +84,10 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
+
   let queryParams = [];
   let queryString = `SELECT properties.*,  AVG (rating) AS average_rating
-  FROM properties JOIN property_reviews ON properties.id = property_id `;
+  FROM properties LEFT JOIN property_reviews ON properties.id = property_id `;
   if(options.owner_id){
     queryParams.push(options.owner_id);
     queryString += `WHERE properties.owner_id = $${queryParams.length}`;
@@ -140,9 +141,16 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const queryString = `INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street,
+    city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms) VALUES ($1, $2, $3, $4,
+      $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *;`
+  const queryParams = [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street,
+    property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms]
+    return pool.query(queryString, queryParams)
+  .then(res => res.rows[0])
+  // const propertyId = Object.keys(properties).length + 1;
+  // property.id = propertyId;
+  // properties[propertyId] = property;
+  // return Promise.resolve(property);
 }
 exports.addProperty = addProperty;
